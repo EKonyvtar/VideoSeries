834c7cbca985630bb9ecbadc097cde798d55388c
Akos Murati <akos@murati.hu>
2019-11-08 02:38:36 +0100

    Add glide for thumbnail loading

--- app/src/main/java/com/murati/videos/adapter/VideoAdapter.java
+++ app/src/main/java/com/murati/videos/adapter/VideoAdapter.java
@@ -3,10 +3,12 @@ package com.murati.videos.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubeThumbnailLoader;
 import com.google.android.youtube.player.YouTubeThumbnailView;
@@ -36,6 +38,13 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHol
     public void onBindViewHolder(VideoViewHolder holder, int position) {
         final Video v = VideoList.get(position);
         holder.title.setText(VideoList.get(position).getTitle());
+
+        Glide
+            .with(holder.itemView)
+            .load(v.getThumbnailImage())
+            //.placeholder(placeholder)
+            .fitCenter()
+            .into(holder.thumbnail2);
 
         holder.thumbnail.initialize(DeveloperKey.DEVELOPER_KEY, new YouTubeThumbnailView.OnInitializedListener() {
             @Override
@@ -72,12 +81,14 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHol
     public class VideoViewHolder extends RecyclerView.ViewHolder {
         public TextView title;
         public YouTubeThumbnailView thumbnail;
+        public ImageView thumbnail2;
 
 
         public VideoViewHolder(View view) {
             super(view);
             title = (TextView) view.findViewById(R.id.text);
             thumbnail = (YouTubeThumbnailView) view.findViewById(R.id.thumbnail);
+            thumbnail2 = (ImageView) view.findViewById(R.id.thumbnail2);
         }
     }
 }
